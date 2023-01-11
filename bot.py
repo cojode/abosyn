c3642a2845b034d324bd3c3fed8a3004addb77b9
Харченко Владимир <69915638+bigvladimir@users.noreply.github.com>
2023-01-11 18:08:35 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ bot = Bot(token="5972708010:AAEqHwGxhzlufz0EdvtUa2s-E9lFUk-zjc0")
 dp = Dispatcher(bot)
 
 
+COOLDOWN = 67
+
+
 context = {}
 
 
@@ -23,23 +26,30 @@ async def get_answer():
 
 @dp.message_handler(commands=["start", "help"])
 async def get_help(message):
-    await message.answer("Используйте /gen для генерации рецепта")
+    await message.answer("Напишите \"хочу рецепт\" или просто \"рецепт\" для генерации рецепта")
 
 
-@dp.message_handler(commands=["gen"])
+@dp.message_handler(content_types=['text'])
 async def get_messages(message):
-    last_command_use_time = check_id_in_context(message.from_user.id)
+    message_text = message.text.lower()
     
-    if (datetime.datetime.now() - last_command_use_time).seconds > 60:
-        context[message.from_user.id] = datetime.datetime.now()
+    if message_text == "хочу рецепт" or message_text == "рецепт":
+        last_command_use_time = check_id_in_context(message.from_user.id)
         
-        received_answer = await get_answer()
-        
-        for x in range(0, len(received_answer), 4096):
-            await message.answer(received_answer[x:x+4096])
-        
-    else:
-        await message.answer("Пожалуйста, пожалейте сервер!\nВремя между запросами минута")
+        if (datetime.datetime.now() - last_command_use_time).seconds > COOLDOWN:
+            context[message.from_user.id] = datetime.datetime.now()
+            
+            await message.answer("Ваш рецепт генерируется...")
+            
+            received_answer = await get_answer()
+            
+            for x in range(0, len(received_answer), 4096):
+                await message.answer(received_answer[x:x+4096])
+            
+        else:
+            await message.answer("Пожалуйста, пожалейте сервер!\nПерерыв между запросами "\
+            + str(COOLDOWN) + " секунд\nМожно вызвать через "\
+            + str(COOLDOWN - (datetime.datetime.now() - last_command_use_time).seconds) + " секунд")
 
 
 if __name__ == "__main__":
