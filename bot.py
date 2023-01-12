f41d41eeadfb52871c71814efc6873412549fbfb
Харченко Владимир <69915638+bigvladimir@users.noreply.github.com>
2023-01-12 19:56:56 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,31 +1,27 @@
 from aiogram import Bot, Dispatcher, executor, types
 import datetime
 import json
+from config_loader import ConfigLoader
 import recipe_model
 
-# test
+
 
 rp = recipe_model.RecipeModel("havai/awesome_recipes")
 
-context = {}
-context_uptime = datetime.datetime.now()
-
-
-
-with open("config.json") as f:
-    config = json.load(f)
-    
-settings = config["settings"]
-lits = config["str_literals"]
-
-
-COOLDOWN = settings["delay"]
-DEFAULT_DATE = datetime.datetime(2010,1,1, 0, 0, 0)
+config = ConfigLoader()
+  
+settings = config.get_section("bot_settings")
+lits = config.get_section("bot_str_literals")
 
 bot = Bot(token=settings["token"])
 dp = Dispatcher(bot)
 
 
+
+context = {}
+context_uptime = datetime.datetime.now()
+
+DEFAULT_DATE = datetime.datetime(2010,1,1, 0, 0, 0)
 
 def check_id_in_context(uid):
     if uid not in context:
@@ -41,6 +37,8 @@ def check_context_uptime():
 
 
 def user_spy(message, text):
+    lits = config.get_section("bot_str_literals", True)
+    
     print(lits["log_mes"]\
     % {"time": datetime.datetime.now(), "name": message.from_user.first_name,\
     "nick": message.from_user.username, "id": message.from_user.id, "text": text})
@@ -48,28 +46,45 @@ def user_spy(message, text):
 
 
 async def get_answer():
+    config.update()
+    settings = config.get_section("bot_settings")
+    lits = config.get_section("bot_str_literals")
+    
     return lits["pecipe_first"] + rp.generate_recipe(max_length=settings["max_length"])
 
-
-
-@dp.message_handler(commands=["start", "help"])
-async def get_help(message:types.Message):
+def update_keyboard():
     keyboard = types.ReplyKeyboardMarkup(
         keyboard=[[types.KeyboardButton(text=lits["gen_com"])],],
         resize_keyboard=True,
         input_field_placeholder=lits["input_place"]
     )
+    return keyboard
+
+
+
+@dp.message_handler(commands=["start", "help"])
+async def get_help(message:types.Message):
+    lits = config.get_section("bot_str_literals", True)
+    
+    keyboard = update_keyboard()
+    
     await message.answer(lits["help_mes"]\
                         % {"com": lits["gen_com"].lower()},\
                         reply_markup=keyboard)
 
 @dp.message_handler(lambda message: message.text.lower() == lits["gen_com"].lower())
 async def get_messages(message:types.Message):
+    config.update()
+    settings = config.get_section("bot_settings")
+    lits = config.get_section("bot_str_literals")
+    
     check_context_uptime()
+    
+    keyboard = update_keyboard()
 
     last_command_use_time = check_id_in_context(message.from_user.id)
     
-    if (datetime.datetime.now() - last_command_use_time).seconds > COOLDOWN:
+    if (datetime.datetime.now() - last_command_use_time).seconds > settings["delay"]:
         context[message.from_user.id] = datetime.datetime.now()
         
         user_spy(message, "generating recipe")
@@ -86,7 +101,7 @@ async def get_messages(message:types.Message):
         
     else:
         await message.answer(lits["delay_mes"]\
-        % {"delay": COOLDOWN, "left": COOLDOWN - (datetime.datetime.now() - last_command_use_time).seconds})
+        % {"delay": settings["delay"], "left": settings["delay"] - (datetime.datetime.now() - last_command_use_time).seconds})
 
 
 
