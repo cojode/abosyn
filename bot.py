cd6f1d8b5230c0442233b02907d08b2e6ce276c9
4dabloon <120025491+4dabloon@users.noreply.github.com>
2023-01-13 07:08:30 +0500

    bot

--- bot.py
+++ bot.py
@@ -1,31 +1,29 @@
-from aiogram import Bot, Dispatcher, executor, types
 import datetime
-import json
-from config_loader import ConfigLoader
+
 import recipe_model
-
-
+from aiogram import Bot, Dispatcher, executor, types
+from config_loader import ConfigLoader
 
 rp = recipe_model.RecipeModel("havai/awesome_recipes")
 
 config = ConfigLoader()
-  
+
 settings = config.get_section("bot_settings")
 
 bot = Bot(token=settings["token"])
 dp = Dispatcher(bot)
 
-
-
 context = {}
 context_uptime = datetime.datetime.now()
 
-DEFAULT_DATE = datetime.datetime(2010,1,1, 0, 0, 0)
+DEFAULT_DATE = datetime.datetime(2010, 1, 1, 0, 0, 0)
+
 
 def check_id_in_context(uid):
     if uid not in context:
         context[uid] = DEFAULT_DATE
     return context[uid]
+
 
 def check_context_uptime():
     global context_uptime
@@ -34,80 +32,79 @@ def check_context_uptime():
         context_uptime = datetime.datetime.now()
 
 
-
 def user_spy(message, text):
     config.update()
     lits = config.get_section("bot_str_literals")
-    
-    print(lits["log_mes"]\
-    % {"time": datetime.datetime.now(), "name": message.from_user.first_name,\
-    "nick": message.from_user.username, "id": message.from_user.id, "text": text})
 
+    print(lits["log_mes"]
+          % {"time": datetime.datetime.now(), "name": message.from_user.first_name, "nick": message.from_user.username,
+             "id": message.from_user.id, "text": text})
 
 
 async def get_answer():
     config.update()
     settings = config.get_section("bot_settings")
     lits = config.get_section("bot_str_literals")
-    
+
     return lits["pecipe_first"] + rp.generate_recipe(max_length=settings["max_length"])
+
 
 def update_keyboard():
     config.update()
     lits = config.get_section("bot_str_literals")
-    
+
     keyboard = types.ReplyKeyboardMarkup(
-        keyboard=[[types.KeyboardButton(text=lits["gen_com"])],],
+        keyboard=[[types.KeyboardButton(text=lits["gen_com"])], ],
         resize_keyboard=True,
         input_field_placeholder=lits["input_place"]
     )
     return keyboard
 
 
-
 @dp.message_handler(commands=["start", "help"])
-async def get_help(message:types.Message):
+async def get_help(message: types.Message):
     config.update()
     lits = config.get_section("bot_str_literals")
-    
+
     keyboard = update_keyboard()
-    
-    await message.answer(lits["help_mes"]\
-                        % {"com": lits["gen_com"].lower()},\
-                        reply_markup=keyboard)
+
+    await message.answer(lits["help_mes"]
+                         % {"com": lits["gen_com"].lower()},
+                         reply_markup=keyboard)
+
 
 @dp.message_handler(lambda message: message.text.lower() == config.get_section("bot_str_literals")["gen_com"].lower())
-async def get_messages(message:types.Message):
+async def get_messages(message: types.Message):
     config.update()
     settings = config.get_section("bot_settings")
     lits = config.get_section("bot_str_literals")
-    
+
     check_context_uptime()
-    
+
     keyboard = update_keyboard()
 
     last_command_use_time = check_id_in_context(message.from_user.id)
-    
+
     if (datetime.datetime.now() - last_command_use_time).seconds > settings["delay"]:
         context[message.from_user.id] = datetime.datetime.now()
-        
+
         user_spy(message, "generating recipe")
-        
+
         try:
             await message.answer(lits["waiting_gen"], reply_markup=keyboard)
-            
+
             received_answer = await get_answer()
             for x in range(0, len(received_answer), 4096):
-                await message.answer(received_answer[x:x+4096], reply_markup=keyboard)
+                await message.answer(received_answer[x:x + 4096], reply_markup=keyboard)
         except:
             context[message.from_user.id] = DEFAULT_DATE
             await message.answer(lits["error_mes"], reply_markup=keyboard)
-        
-    else:
-        await message.answer(lits["delay_mes"]\
-        % {"delay": settings["delay"], "left": settings["delay"] - (datetime.datetime.now() - last_command_use_time).seconds},\
-                reply_markup=keyboard)
 
+    else:
+        await message.answer(lits["delay_mes"]
+                             % {"delay": settings["delay"],
+                                "left": settings["delay"] - (datetime.datetime.now() - last_command_use_time).seconds},
+                             reply_markup=keyboard)
 
 
 if __name__ == "__main__":
